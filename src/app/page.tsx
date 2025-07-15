8fa398d79fec8fa03a2225e2d81c13b1ff9e1e70
antho <anthonynuge0509@gmail.com>
2025-07-15 14:52:39 -0500

    Add full background hero section and image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import CardsSection from "@/components/CardsSection";
-import HeroSection from "@/components/HeroSection";
-import PageWrapper from "@/components/PageWrapper";
+import HeroSection from "@/components/home/HeroSection";
 import SecureSection from "@/components/SecureSection";
 import ServicesSection from "@/components/ServicesSection";
 
 export default function Home() {
   return (
-    <PageWrapper>
+    <>
       <HeroSection />
       <ServicesSection />
       <SecureSection />
       <CardsSection />
-    </PageWrapper>
+    </>
   );
 }
---
+++ src/components/home/HeroSection.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import ButtonCrossArrow from "../shared/ButtonCrossArrow";
+export default function Hero() {
+  return (
+    <section className="relative overflow-hidden w-full h-screen text-white">
+      <Image
+        src="/home/hero.jpg"
+        alt="Hero image"
+        fill
+        className="object-cover object-center -z-10"
+        priority
+      />
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/30 -z-9" />
+      <div className="relative z-20 flex h-[85%] w-full px-6 md:px-16 py-12">
+        <div className="flex flex-col md:flex-row justify-between w-full h-full max-w-7xl mx-auto">
+          {/* Left - Heading */}
+          <div className="flex items-start md:items-start justify-start md:justify-start w-full md:w-1/2 pt-24">
+            <h1 className="text-4xl md:text-8xl font-bold leading-tight tracking-tight">
+              Power
+              <br />
+              Reimagined
+              <span className="text-xl align-super ml-1">™</span>
+            </h1>
+          </div>
+
+          {/* Right - Description aligned bottom right */}
+          <div className="flex flex-col items-end justify-end w-full md:w-1/2 pb-12 gap-4">
+            <p className="text-lg md:text-2xl max-w-md text-right font-bold">
+              We simplify energy decisions so your business can operate smarter,
+              faster, and more efficiently.
+            </p>
+            <ButtonCrossArrow text="Contact me" href="/contact" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
