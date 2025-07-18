7da223f3ef8940924a5dfe41425b0cc6f7177ce1
antho <anthonynuge0509@gmail.com>
2025-07-18 18:43:27 -0500

    Add case study section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,25 @@ import OurCustomersSection from "@/components/home/OurCustomersSection";
 import StatsSection from "@/components/home/StatsSection";
 import PageWrapper from "@/components/layout/PageWrapper";
 import EmpowerSection from "@/components/home/EmpowerSection";
+import SolutionsSection from "@/components/services/Solutions";
+import CaseStudySection from "@/components/shared/CaseStudySection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-
-      <PageWrapper>
+      <PageWrapper className="mt-24 md:mt-32 space-y-24 md:space-y-52">
         <ServiceSection />
         <StatsSection />
       </PageWrapper>
       <OurCustomersSection />
-      <PageWrapper>
-        <SecureSection />
+      <PageWrapper className="space-y-24 md:space-y-52">
+        {/* <SecureSection /> */}
+        <SolutionsSection />
+        {/* <EmpowerSection /> */}
+      </PageWrapper>
+      <CaseStudySection />
+      <PageWrapper className="space-y-24 md:space-y-52">
         <EmpowerSection />
       </PageWrapper>
     </>
