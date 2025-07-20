04c1cea46d37839a61772bed80fb5bcdc41840d8
antho <anthonynuge0509@gmail.com>
2025-07-19 19:18:30 -0500

    Fix margin with new format.

--- src/components/about/TeamSection.tsx
+++ src/components/about/TeamSection.tsx
@@ -3,7 +3,7 @@ import { teamGroups } from "./constants";
 
 export default function TeamSection() {
   return (
-    <div className="max-w-7xl mx-auto space-y-32">
+    <div className="max-w-7xl mx-auto space-y-32 mb-32">
       {teamGroups.map((group) => (
         <div key={group.group} className="space-y-10">
           <p className="ml-0 md:ml-24 text-lg text-gray-500 uppercase tracking-wide mb-1">
