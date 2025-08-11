23dbf33265410635f8a311921024d5434351d2d5
antho <anthonynuge0509@gmail.com>
2025-08-11 16:29:36 -0500

    Add loader spinner to improve performance.

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white dark:bg-neutral-900">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-primary border-t-transparent" />
+    </div>
+  );
+}
